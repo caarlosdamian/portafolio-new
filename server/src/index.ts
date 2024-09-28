750e0b494c17691fcac73223f11040923698c260
caarlosdamian <dapecarlos@gmail.com>
2024-09-28 13:43:12 -0600

    fix deploy

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import express, { json } from 'express';
 import cors from 'cors'
-import router from './src/routes';
+import router from './routes';
 
 const app = express();
 const port = process.env.PORT || 8080
