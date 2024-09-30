8d1ca4e57cee8e8fa8b41ab0841881bc7c9a44c2
caarlosdamian <dapecarlos@gmail.com>
2024-09-29 18:02:37 -0600

    test log

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -4,14 +4,23 @@ import { authHandler, authStatus } from '../controllers/auth.controller';
 
 const router = Router();
 
-router.post('/', passport.authenticate('local'), authHandler);
+router.post(
+  '/',
+  (req, _res, next) => {
+    console.log(req);
+    next();
+  },
+  passport.authenticate('local'),
+  authHandler
+);
 router.get('/status', authStatus);
-router.post('/logout', function(req, res, next){
-  req.logout(function(err) {
-    if (err) { return next(err); }
+router.post('/logout', function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
     res.json('Se deslogweo');
   });
 });
 
-
-export default router
+export default router;
