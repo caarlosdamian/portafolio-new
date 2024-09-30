55cf0251539f51e71b2d453275d910b67a5174a4
caarlosdamian <dapecarlos@gmail.com>
2024-09-30 14:00:02 -0600

    add new mongostore

--- server/dist/index.js
+++ server/dist/index.js
@@ -32,27 +32,28 @@ const routes_1 = __importDefault(require("./routes"));
 const connectDb_1 = require("./utils/connectDb");
 const dotenv = __importStar(require("dotenv"));
 const passport_1 = __importDefault(require("passport"));
-const express_session_1 = __importStar(require("express-session"));
+const express_session_1 = __importDefault(require("express-session"));
+const connect_mongo_1 = __importDefault(require("connect-mongo"));
 require("./strategies/local-strategy");
+const mongoose_1 = __importDefault(require("mongoose"));
 dotenv.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8080;
 app.use((0, express_1.json)());
 app.use((0, cors_1.default)());
+(0, connectDb_1.connectDb)();
 app.use((0, express_session_1.default)({
-    secret: process.env.SECRET || '',
+    secret: process.env.SECRET || 'your-secret-key',
     saveUninitialized: false,
     resave: false,
-    store: new express_session_1.MemoryStore({}),
     cookie: {
-        secure: false, // Set true if using HTTPS
-        maxAge: 1000 * 60 * 60 * 24 // Session expiration time in milliseconds (1 day here)
-    }
+        maxAge: 60000 * 60,
+    },
+    store: connect_mongo_1.default.create({ client: mongoose_1.default.connection.getClient() }),
 }));
 app.use(passport_1.default.initialize());
-app.use(passport_1.default.session()); // attaching user prop to session
+app.use(passport_1.default.session()); // Attach user property to session
 app.use(routes_1.default);
 app.listen(port, () => {
     console.log(`App started at http://localhost:${port}`);
-    (0, connectDb_1.connectDb)();
 });
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,37 +1,40 @@
 import express, { json } from 'express';
-import cors from 'cors'
+import cors from 'cors';
 import router from './routes';
 import { connectDb } from './utils/connectDb';
 import * as dotenv from 'dotenv';
 import passport from 'passport';
-import session, { MemoryStore } from 'express-session'
-import './strategies/local-strategy'
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
+
+import './strategies/local-strategy';
+import mongoose from 'mongoose';
+
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
 
 app.use(json());
-app.use(cors())
+app.use(cors());
+connectDb();
 
 app.use(
   session({
-    secret: process.env.SECRET || '' ,
+    secret: process.env.SECRET || 'your-secret-key',
     saveUninitialized: false,
     resave: false,
-    store: new MemoryStore({
-    }),
     cookie: {
-      secure: false,  // Set true if using HTTPS
-      maxAge: 1000 * 60 * 60 * 24  // Session expiration time in milliseconds (1 day here)
-    }
+      maxAge: 60000 * 60,
+    },
+    store: MongoStore.create({ client: mongoose.connection.getClient() }),
   })
 );
+
 app.use(passport.initialize());
-app.use(passport.session()); // attaching user prop to session
-app.use(router)
+app.use(passport.session()); // Attach user property to session
+app.use(router);
 
 app.listen(port, () => {
-  console.log(`App started at http://localhost:${port}`)
-  connectDb();
+  console.log(`App started at http://localhost:${port}`);
 });
