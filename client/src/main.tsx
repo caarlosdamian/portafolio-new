0c1a4a118d7650f863867b135c9e745d7805640e
caarlosdamian <dapecarlos@gmail.com>
2024-10-16 22:04:53 -0600

    feat(styling): added tailwind css

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById('root')!).render(<StrictMode>
+  <h1 className='text-7xl text-red-700'>Testing tailwindcss</h1>
+</StrictMode>);
